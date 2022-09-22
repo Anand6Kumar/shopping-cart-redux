cf786a2d8a204b16f34a656a2159f5fd059990a7
Anand6Kumar <anand_kumar6@persistent.com>
2022-09-22 18:24:42 +0530

    Header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-// import { Link } from 'react-router-dom'
  const Header = ()=>{
     return(
       <Navbar bg="dark" variant="dark">
